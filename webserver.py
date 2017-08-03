100209782c619fddb09ce7be5b22ecfb59c15670
cowalla <wallaconno@gmail.com>
2017-08-03 14:33:15 -0700

    make it serve static files

--- webserver.py
+++ webserver.py
@@ -1,28 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 
 
-app = Flask(__name__)
-
-GOPRO_NAME = 'cyclops1'
-GOPRO_URL = 'http://10.5.5.9:8080/videos/DCIM/100GOPRO/'
-SAVE_DIR = '/Users/wallacdc/Desktop/PhotoBooth/images/'
+app = Flask(__name__, static_folder='templates/assets', static_url_path='/static')
 
 
 @app.route('/booth', methods=['GET','POST'])
 def booth():
     if request.method == 'GET':
-
-        return render_template(
-            'booth.html'
-        )
-
+        return render_template('booth.html')
     elif request.method == 'POST':
         from settings import BOOTH
         BOOTH.take_photo()
 
-        return render_template(
-            'booth.html'
-        )
+        return render_template('booth.html')
+
+
+@app.route('/slideshow', methods=['GET'])
+def slideshow():
+    return render_template('slideshow.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
