b323ccc5c30c43c28bcbcc0fe3a1e375d6ae5747
Wallace, David <walldc@amazon.com>
2017-08-03 15:36:05 -0700

    complete pull from S3.

--- templates/assets/s3.js
+++ templates/assets/s3.js
@@ -14,35 +14,25 @@ var s3 = new AWS.S3({
   params: {Bucket: albumBucketName}
 });
 
-var params = {
-  Bucket: 'groove-booth', /* required */
-};
+var params = 
 
-var currentFiles = []
+var currentFiles = []  // All images currently in slideshow
 
-function encode(data)
-{
-    var str = data.reduce(function(a,b){ return a+String.fromCharCode(b) },'');
-    return btoa(str).replace(/.{76}(?=.)/g,'$&\n');
+function addImageToSlideShow(image_name) {
+
 }
 
-s3.listObjects(params, function(err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else {
-    var availableFiles = data.Contents;  // successful response
-    for (i=0; i<availableFiles.length; i++) {
-      if (currentFiles.indexOf(availableFiles[i].Key) === -1) {
-        currentFiles.push(availableFiles[i].Key);
-        console.log(currentFiles)
-        s3.getObject({Key: availableFiles[i].Key},function(err,file){
-          $timeout(
-            function(){
-              $scope.s3url = "data:image/jpeg;base64," + encode(file.Body);
-            },
-            1
-          )
-        });
+s3.listObjects(
+  function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else {
+      var availableFiles = data.Contents;  // successful response
+      for (i=0; i<availableFiles.length; i++) {
+        if (currentFiles.indexOf(availableFiles[i].Key) === -1) {
+          currentFiles.push(availableFiles[i].Key);
+
+        };
       };
     };
-  };
-};
+});
+
