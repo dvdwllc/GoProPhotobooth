02b375496d26ed904cb7cb3813657b9979ff5593
cowalla <wallaconno@gmail.com>
2017-08-03 14:16:39 -0700

    move booth to settings

---
+++ settings.py
@@ -0,0 +1,7 @@
+from GoProPhotoBooth import GoProPhotoBooth
+
+
+GOPRO_NAME = 'cyclops1'
+GOPRO_URL = 'http://10.5.5.9:8080/videos/DCIM/100GOPRO/'
+SAVE_DIR = '/Users/wallacdc/Desktop/PhotoBooth/images/'
+BOOTH = GoProPhotoBooth(GOPRO_NAME, GOPRO_URL, SAVE_DIR)
--- webserver.py
+++ webserver.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request
-from GoProPhotoBooth import GoProPhotoBooth
+
 
 app = Flask(__name__)
 
-Booth = GoProPhotoBooth('cyclops1',
-                        'http://10.5.5.9:8080/videos/DCIM/100GOPRO/',
-                        '/Users/wallacdc/Desktop/PhotoBooth/images/')
+GOPRO_NAME = 'cyclops1'
+GOPRO_URL = 'http://10.5.5.9:8080/videos/DCIM/100GOPRO/'
+SAVE_DIR = '/Users/wallacdc/Desktop/PhotoBooth/images/'
+
 
 @app.route('/booth', methods=['GET','POST'])
 def booth():
@@ -16,8 +17,8 @@ def booth():
         )
 
     elif request.method == 'POST':
-
-        Booth.take_photo()
+        from settings import BOOTH
+        BOOTH.take_photo()
 
         return render_template(
             'booth.html'
