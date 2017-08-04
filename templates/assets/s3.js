076a719026bca20a68506a97cdbf31b3233a1edb
Wallace, David <walldc@amazon.com>
2017-08-03 21:55:27 -0700

    completed client-side javascript.

--- templates/assets/s3.js
+++ templates/assets/s3.js
@@ -14,25 +14,47 @@ var s3 = new AWS.S3({
   params: {Bucket: albumBucketName}
 });
 
-var params = 
-
 var currentFiles = []  // All images currently in slideshow
 
-function addImageToSlideShow(image_name) {
+function getNewImages() {
+  s3.listObjects(
+    {Bucket: albumBucketName}, 
+    function(err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else {
 
-}
+        var availableFiles = data.Contents;  // successful response
 
-s3.listObjects(
-  function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else {
-      var availableFiles = data.Contents;  // successful response
-      for (i=0; i<availableFiles.length; i++) {
-        if (currentFiles.indexOf(availableFiles[i].Key) === -1) {
-          currentFiles.push(availableFiles[i].Key);
+        for (i=0; i<availableFiles.length; i++) {
 
+          if (currentFiles.indexOf(availableFiles[i].Key) === -1) {
+            currentFiles.push(availableFiles[i].Key);
+
+            var img_div = jQuery(
+              "<div></div>", 
+              {
+                class: "mySlides fade"
+              }
+            );
+
+            var img_link = jQuery(
+              "<img></img>", 
+              {
+                src: "http://s3-us-west-2.amazonaws.com/groove-booth/" + availableFiles[i].Key,
+                style: "width:100%"
+              }
+            );
+
+            img_link.appendTo(img_div)
+            img_div.appendTo($(".slideshow-container"))
+
+            console.log(img_div)
+          };
         };
+        console.log(currentFiles);
       };
-    };
-});
+  });
+};
+
+getNewImages();
 
